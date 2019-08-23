29acf758f2f3f5bb597117c6783d6faf174cd7d7
Pulkit Singhal <pulkitsinghal@gmail.com>
2019-08-23 14:42:43 -0700

    Used the UI library inside a proof-of-concept tester application
    ... close issue #4

--- projects/checkbox-mcq/src/lib/checkbox-mcq.component.ts
+++ projects/checkbox-mcq/src/lib/checkbox-mcq.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+/**
+ * Poll and Option are being used as interface names
+ * instead of Question and Answer because
+ * a question does not house an answer inside it
+ * but a poll can house an option inside of it.
+ * 
+ * This technicality of what the words mean in the english language
+ * has led to the naming choices you see here.
+ */
+export interface Poll { // synonymous to a question
+  content: string;
+  options: Array<Option>;
+}
+
+export interface Option { //synonymous to an answer
+  content: string;
+  correct: boolean;
+}
 
 @Component({
   selector: 'ngx-google-forms-checkbox-mcq',
-  template: `
-    <p>
-      checkbox-mcq works!
-    </p>
-  `,
-  styles: []
+  templateUrl: './checkbox-mcq.component.html',
+  styleUrls: ['./checkbox-mcq.component.css']
 })
 export class CheckboxMcqComponent implements OnInit {
+
+  @Input()
+  poll: Poll;
 
   constructor() { }
 
